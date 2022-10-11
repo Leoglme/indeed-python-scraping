0108d4fb6212e544a36dfa8160639631656d7635
Léo Guillaume <leo.guillaume@epitech.eu>
2022-10-11 15:56:42 +0200

    fix add db for get types jobs

--- indeed_types_job.py
+++ indeed_types_job.py
@@ -7,24 +7,23 @@ class IndeedTypesJob:
     alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u',
                 'v', 'w', 'x', 'y', 'z']
     database = Database()
-    types_job = []
 
     def __init__(self):
+        self.database.delete_all_job_types()
         for letter in self.alphabet:
             self.get_types_job(letter)
-        self.save_jobs()
 
     def get_types_job(self, letter: str):
         url = f'https://autocomplete.indeed.com/api/v0/suggestions/what?country=FR&language=fr&count=100000000' \
-              f'&formatted=1&query={letter}&useEachWord=false&page=serp&showAlternateSuggestions=false&merged=true&rich=true '
-        r = requests.get(url)
-        self.types_job += json.loads(r.content)
+              f'&formatted=1&query={letter}&useEachWord=false&page=serp&showAlternateSuggestions=false&merged=true&rich=true'
 
-    def save_jobs(self):
-        self.database.delete_all_job_types()
-        for type_job in self.types_job:
+        r = requests.get(url)
+        self.save_jobs(json.loads(r.content))
+        print(json.loads(r.content))
+
+    def save_jobs(self, jobs):
+        for type_job in jobs:
             self.database.add_job_type(type_job["suggestion"], type_job["payload"]["score"])
-        self.database.close_connect()
 
 
 indeed_types_job = IndeedTypesJob()
